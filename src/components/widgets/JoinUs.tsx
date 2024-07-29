553e0a849c43c6b1fa4fad5815bca6387ecb7fe9
Johan Winarto <johanpbrc@gmail.com>
2024-07-29 21:30:20 +0700

    Update JoinUs Page Content

--- src/components/widgets/JoinUs.tsx
+++ src/components/widgets/JoinUs.tsx
@@ -15,14 +15,17 @@ const JoinUs = ({ id, hasBackground }: JoinProps) => {
                 Recruitment
               </h1>
               <p className="mb-6 text-md font-normal text-gray-600 dark:text-slate-400">
-                Take control of Indonesia virtual airspace and ensure a <span className="font-semibold underline decoration-primary-600 decoration-wavy decoration-1 underline-offset-2">Smooth, Safe, and Efficient</span>{' '} flow of aircraft
-                within the designated Flight Information Region (FIR). Join forces with a team of highly skilled online
-                professionals dedicated to excellence in air traffic control. <br />
+                Take control of Indonesia virtual airspace and ensure a{' '}
+                <span className="font-semibold underline decoration-primary-600 decoration-wavy decoration-1 underline-offset-2">
+                  Smooth, Safe, and Efficient
+                </span>{' '}
+                flow of aircraft within the designated Flight Information Region (FIR). Join forces with a team of
+                highly skilled online professionals dedicated to excellence in air traffic control. <br />
                 <br />
                 Our comprehensive training program prepares trainees to meet the highest standards of the VATSIM Global
-                Rating Policy, with a strong emphasis on local procedures. Navigate through Indonesia dynamic and
-                challenging airspace with confidence. Are you ready to take on the challenge? Explore further details
-                below!
+                Controller Administration Policy (GCAP), with a strong emphasis on local procedures. Navigate through
+                Indonesia dynamic and challenging airspace with confidence. Are you ready to take on the challenge?
+                Explore further details below!
               </p>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <div className="bg-white rounded-lg shadow-md p-4 dark:bg-[#1e293b]">
@@ -36,6 +39,18 @@ const JoinUs = ({ id, hasBackground }: JoinProps) => {
                   {/* <p className="mt-2">Until Further Notice</p> */}
                 </div>
               </div>
+              {/* <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
+                <div className="bg-white rounded-lg shadow-md p-4 dark:bg-[#1e293b]">
+                  <h3 className="text-xl font-bold mb-2">RESIDENT CONTROLLER</h3>
+                  <div className="bg-green-500 text-white rounded-lg px-4 py-2">OPEN</div> */}
+              {/* <p className="mt-2">Until Further Notice</p> */}
+              {/* </div>
+                <div className="bg-white rounded-lg shadow-md p-4 dark:bg-[#1e293b]">
+                  <h3 className="text-xl font-bold mb-2">VISITING CONTROLLER</h3>
+                  <div className="bg-green-500 text-white rounded-lg px-4 py-2">OPEN</div> */}
+              {/* <p className="mt-2">Until Further Notice</p> */}
+              {/* </div>
+              </div> */}
             </div>
           </div>
         </div>
